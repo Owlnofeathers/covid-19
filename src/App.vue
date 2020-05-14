3fe207eca3dc7b34e6345bfe4c86a1dd5b6b27c3
Adam Bailey <adamjbailey7@gmail.com>
2020-05-14 08:23:06 -0700

    initial layout and data fetching

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container mx-auto px-10">
+      <img
+        class="object-contain h-48 w-full mt-10"
+        alt="Image of Washington State with Clark County Highlighted"
+        src="./assets/washington_highlighting_clark_county.png"
+      />
+      <h1 class="text-5xl text-gray-600 font-extrabold text-center">
+        COVID-19
+      </h1>
+      <DisplayData
+        v-if="stateData"
+        :title="`Current data for ${stateData.stateName} State`"
+        :data="stateData"
+        class="my-10"
+      ></DisplayData>
+      <DisplayData
+        v-if="countyData"
+        :title="
+          `Current data for ${countyData.countyName} County, ${countyData.stateName}`
+        "
+        :data="countyData"
+        class="my-10"
+      ></DisplayData>
+      {{ countyData }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import DisplayData from "./components/DisplayData";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: { DisplayData },
+  data() {
+    return {
+      stateData: null,
+      countyData: null
+    };
+  },
+  created() {
+    this.fetchCountyData();
+    this.fetchStateData();
+  },
+  methods: {
+    fetchCountyData() {
+      return axios
+        .get(
+          "https://data.covidactnow.org/latest/us/counties/53011.NO_INTERVENTION.json"
+        )
+        .then(response => {
+          this.countyData = response.data;
+        });
+    },
+    fetchStateData() {
+      return axios
+        .get(
+          "https://data.covidactnow.org/latest/us/states/WA.NO_INTERVENTION.json"
+        )
+        .then(response => {
+          this.stateData = response.data;
+        });
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/DisplayData.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h3 class="text-2xl text-gray-800 font-bold text-center">{{ title }}</h3>
+    <p class="text-xl mt-10">
+      <span class="text-gray-600 font-bold mr-2">Population:</span>
+      <span class="text-gray-800 font-bold">{{
+        data.population.toLocaleString()
+      }}</span>
+    </p>
+    <p class="text-xl">
+      <span class="text-gray-600 font-bold mr-2"
+        >Current Cumulative Cases:</span
+      >
+      <span class="text-gray-800 font-bold">{{
+        data.actuals.cumulativeConfirmedCases.toLocaleString()
+      }}</span>
+    </p>
+    <p class="text-xl">
+      <span class="text-gray-600 font-bold mr-2"
+        >Current Cumulative Deaths:</span
+      >
+      <span class="text-gray-800 font-bold">{{
+        data.actuals.cumulativeDeaths.toLocaleString()
+      }}</span>
+    </p>
+    <p class="text-xl">
+      <span class="text-gray-600 font-bold mr-2"
+        >Current Cumulative Positive Tests:</span
+      >
+      <span class="text-gray-800 font-bold">{{ positiveTests }}</span>
+    </p>
+    <p class="text-xl">
+      <span class="text-gray-600 font-bold mr-2"
+        >Current Cumulative Negative Tests:</span
+      >
+      <span class="text-gray-800 font-bold">{{ negativeTests }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayData",
+  props: {
+    title: {
+      required: true,
+      type: String
+    },
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    positiveTests() {
+      return this.data.actuals.cumulativePositiveTests
+        ? this.data.actuals.cumulativePositiveTests.toLocaleString()
+        : "None Reported";
+    },
+    negativeTests() {
+      return this.data.actuals.cumulativeNegativeTests
+        ? this.data.actuals.cumulativeNegativeTests.toLocaleString()
+        : "None Reported";
+    }
+  }
+};
+</script>
+
+<style scoped></style>
