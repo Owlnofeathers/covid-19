f9ae429a99c78ba6c8ce9d0e3cc956c9d7740851
Adam Bailey <adamjbailey7@gmail.com>
2020-05-15 09:08:48 -0700

    fetch state data when choosing new county, handle errors

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,43 @@
 <template>
   <div id="app">
-    <div class="container mx-auto px-10 pb-32">
-      <img
-        class="object-contain h-32 w-full mt-10"
-        alt="Image of Washington State with Clark County Highlighted"
-        src="./assets/washington_highlighting_clark_county.png"
-      />
-      <h1 class="text-5xl text-gray-600 font-extrabold text-center mb-5">
-        COVID-19 By US County
+    <div class="container mx-auto px-10 pb-10">
+      <h1
+        class="text-3xl text-gray-800 font-extrabold text-center my-5 md:mt-20 md:text-5xl"
+      >
+        COVID-19 Totals By US County
       </h1>
       <div class="md:text-center md:px-32">
-        <p class="text-left text-grey-600 font-bold">Type your county here:</p>
         <vue-simple-suggest
-          v-model="chosenCounty"
+          placeholder="Enter your county..."
           :list="countyCodes"
           display-attribute="name"
-          value-attribute="code"
           :filter-by-query="true"
-        >
-          <!-- Filter by input text to only show the matching results -->
-        </vue-simple-suggest>
+          @select="onSelect"
+        ></vue-simple-suggest>
+        <p v-if="error" class="text-red-700 text-sm">{{ error }}</p>
         <p v-if="stateData" class="mt-3 mb-5 text-sm">
           Last Updated: {{ new Date(stateData.lastUpdatedDate).toDateString() }}
         </p>
       </div>
       <div class="md:px-32 md:flex">
-        <DisplayData
-          v-if="stateData"
-          :title="`${stateData.stateName} State`"
-          :data="stateData"
-          class="md:mr-10"
-        ></DisplayData>
-        <DisplayData
-          v-if="countyData"
-          :title="`${countyData.countyName} County, ${countyData.stateName}`"
-          :data="countyData"
-          class="mt-5 md:mt-0"
-        ></DisplayData>
+        <transition name="fade">
+          <DisplayData
+            v-if="stateData"
+            :title="`${stateData.stateName} State`"
+            :data="stateData"
+            class="md:mr-10"
+          ></DisplayData>
+        </transition>
+        <transition name="fade">
+          <DisplayData
+            v-if="countyData"
+            :title="`${countyData.countyName} County, ${countyData.stateName}`"
+            :data="countyData"
+            class="mt-5 md:mt-0"
+          ></DisplayData>
+        </transition>
       </div>
-      <p class="mb-3">
-        This site was created and is maintained by
-        <a href="https://adambailey.io/" class="link">Adam Bailey</a>
-        for an easy place to check the covid-19 totals in Clark County.
-      </p>
-      <p>
-        The data is aggregated by
-        <a href="https://covidactnow.org/" class="link">CovidActNow.org</a>
-        and pulled in via their
-        <a
-          href="https://github.com/covid-projections/covid-data-model/blob/master/api/README.V1.md"
-          class="link"
-          >Official API</a
-        >.
-      </p>
-      <p class="text-gray-600">
-        "Covid Act Now is a multidisciplinary team of technologists,
-        epidemiologists, health experts, and public policy leaders working to
-        provide disease intelligence and data analysis on COVID in the U.S"
-      </p>
+      <Footer></Footer>
     </div>
   </div>
 </template>
@@ -66,45 +46,113 @@
 import axios from "axios";
 import DisplayData from "./components/DisplayData";
 import VueSimpleSuggest from "vue-simple-suggest";
-import "vue-simple-suggest/dist/styles.css"; // Using a css-loader
+import "vue-simple-suggest/dist/styles.css";
+import Footer from "./components/Footer"; // Using a css-loader
 
 export default {
   name: "App",
-  components: { DisplayData, VueSimpleSuggest },
+  components: { Footer, DisplayData, VueSimpleSuggest },
   data() {
     return {
       stateData: null,
       countyData: null,
       countyCodes: [],
-      stateCodes: null,
-      chosenCounty: null
+      chosenCounty: null,
+      error: null
     };
   },
-  async created() {
-    await this.fetchCountyData();
-    await this.fetchStateData();
-    await this.fetchCountyCodes();
+  created() {
+    this.fetchCountyData();
+    this.fetchStateData();
+    this.fetchCountyCodes();
+  },
+  computed: {
+    states() {
+      // an array of state abbreviations and their FIPS code
+      return [
+        { name: "AL", id: "01" },
+        { name: "AK", id: "02" },
+        { name: "AZ", id: "04" },
+        { name: "AR", id: "05" },
+        { name: "CA", id: "06" },
+        { name: "LA", id: "22" },
+        { name: "KY", id: "21" },
+        { name: "CO", id: "08" },
+        { name: "CT", id: "09" },
+        { name: "DE", id: "10" },
+        { name: "DC", id: "11" },
+        { name: "FL", id: "12" },
+        { name: "GA", id: "13" },
+        { name: "HI", id: "15" },
+        { name: "ID", id: "16" },
+        { name: "IL", id: "17" },
+        { name: "IN", id: "18" },
+        { name: "IA", id: "19" },
+        { name: "KS", id: "20" },
+        { name: "ME", id: "23" },
+        { name: "MD", id: "24" },
+        { name: "MA", id: "25" },
+        { name: "MI", id: "26" },
+        { name: "MN", id: "27" },
+        { name: "MS", id: "28" },
+        { name: "MO", id: "29" },
+        { name: "MT", id: "30" },
+        { name: "NE", id: "31" },
+        { name: "NV", id: "32" },
+        { name: "NH", id: "33" },
+        { name: "NJ", id: "34" },
+        { name: "NM", id: "35" },
+        { name: "NY", id: "36" },
+        { name: "NC", id: "37" },
+        { name: "ND", id: "38" },
+        { name: "OH", id: "39" },
+        { name: "OK", id: "40" },
+        { name: "OR", id: "41" },
+        { name: "PA", id: "42" },
+        { name: "RI", id: "44" },
+        { name: "SC", id: "45" },
+        { name: "SD", id: "46" },
+        { name: "TN", id: "47" },
+        { name: "TX", id: "48" },
+        { name: "UT", id: "49" },
+        { name: "VT", id: "50" },
+        { name: "VA", id: "51" },
+        { name: "WA", id: "53" },
+        { name: "WV", id: "54" },
+        { name: "WI", id: "55" },
+        { name: "WY", id: "56" },
+        { name: "PR", id: "72" }
+      ];
+    }
   },
   methods: {
-    fetchCountyData() {
+    fetchCountyData(fipscode = "53011") {
       return axios
         .get(
-          "https://data.covidactnow.org/latest/us/counties/53011.NO_INTERVENTION.json"
+          `https://data.covidactnow.org/latest/us/counties/${fipscode}.NO_INTERVENTION.json`
         )
+        .catch(() => {
+          this.error =
+            "We either weren't able to get data for this county, or no cases have yet been confirmed.";
+        })
         .then(response => {
-          this.countyData = response.data;
+          if (response && response.status === 200) {
+            this.countyData = response.data;
+            this.error = null;
+          }
         });
     },
-    fetchStateData() {
+    fetchStateData(state = "WA") {
       return axios
         .get(
-          "https://data.covidactnow.org/latest/us/states/WA.NO_INTERVENTION.json"
+          `https://data.covidactnow.org/latest/us/states/${state}.NO_INTERVENTION.json`
         )
         .then(response => {
           this.stateData = response.data;
         });
     },
     fetchCountyCodes() {
+      // grab a csv of counties and FIPS from census.gov and turn it into an array of objects
       return axios
         .get("https://api.census.gov/data/2010/dec/sf1?get=NAME&for=county:*")
         .then(async response => {
@@ -114,9 +162,20 @@ export default {
           const flatArray = Array.from(array.flat());
           // make an array of objects with assigned keys
           flatArray.forEach(i => {
-            this.countyCodes.push({ ["name"]: i[0], ["code"]: i[1] + i[2] });
+            this.countyCodes.push({ ["name"]: i[0], ["id"]: i[1] + i[2] });
           });
         });
+    },
+    onSelect(item) {
+      this.countyData = null;
+      this.stateData = null;
+      this.chosenCounty = item.id;
+      this.fetchCountyData(this.chosenCounty);
+      const state = this.states.filter(
+        // get state FIPS from first 2 numbers in county FIPS
+        state => state.id === this.chosenCounty.substring(0, 2)
+      );
+      this.fetchStateData(state[0].name);
     }
   }
 };
@@ -128,5 +187,15 @@ export default {
 
 .link:hover {
   text-decoration: underline;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p class="mt-32">
+      This site was created and is maintained by
+      <a href="https://adambailey.io/" class="link">Adam Bailey</a>
+      for an easy place to check the covid-19 totals by county.
+    </p>
+    <p>
+      The data is aggregated by
+      <a href="https://covidactnow.org/" class="link">CovidActNow.org</a>
+      and pulled in via their
+      <a
+        href="https://github.com/covid-projections/covid-data-model/blob/master/api/README.V1.md"
+        class="link"
+        >Official API</a
+      >.
+    </p>
+    <p class="text-gray-600">
+      "Covid Act Now is a multidisciplinary team of technologists,
+      epidemiologists, health experts, and public policy leaders working to
+      provide disease intelligence and data analysis on COVID in the U.S"
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
